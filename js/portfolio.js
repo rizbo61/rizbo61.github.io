edb8145bf57ab4fab62251540caa262855182ab5
rizbo61 <boris.muminovic@gmail.com>
2016-09-07 19:50:41 +0200

    v 0.63

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,52 +1,57 @@
 $(document).ready(function() {
 
+  /*$("button").click(function() {
+    $("#description").show().children().hide();
+  })*/
+  
+
   $("#web-development").click(function() {
     if ($("#web-development-description").css("display") === "none") {
       $("#skills").css("padding", "0");
-      $("#description").fadeIn(300).children().fadeOut(0);
-      $("#web-development-description").fadeIn(300);
+      $("#description").show().children().hide();
+      $("#web-development-description").fadeIn(500);
     } else {
-        $("#web-development-description").fadeOut(300);
+        $("#web-development-description").fadeOut(500);
     }
   })
 
   $("#html").click(function() {
     if ($("#html-description").css("display") === "none") {
       $("#skills").css("padding", "0");
-      $("#description").fadeIn(300).children().fadeOut(0);
-      $("#html-description").fadeIn(300);
+      $("#description").show().children().hide();
+      $("#html-description").fadeIn(500);
     } else {
-      $("#html-description").fadeOut(300);
+      $("#html-description").fadeOut(500);
     }
   })
 
    $("#css").click(function() {
     if ($("#css-description").css("display") === "none") { 
       $("#skills").css("padding", "0");
-      $("#description").fadeIn(300).children().fadeOut(0);
-      $("#css-description").fadeIn(300);
+      $("#description").show().children().hide();
+      $("#css-description").fadeIn(500);
     } else {
-        $("#css-description").fadeOut(300);
+        $("#css-description").fadeOut(500);
     }
   })
 
   $("#javascript").click(function() {
     if ($("#javascript-description").css("display") === "none") {
       $("#skills").css("padding", "0");
-      $("#description").fadeIn(300).children().fadeOut(0);
-      $("#javascript-description").fadeIn(300);
+      $("#description").show().children().hide();
+      $("#javascript-description").fadeIn(500);
     } else {
-        $("#javascript-description").fadeOut(300);
+        $("#javascript-description").fadeOut(500);
     }
   })
 
   $("#jquery").click(function() {
     if ($("#jquery-description").css("display") === "none") {
       $("#skills").css("padding", "0");
-      $("#description").fadeIn(300).children().fadeOut(0);
-      $("#jquery-description").fadeIn(300);
+      $("#description").show().children().hide();
+      $("#jquery-description").fadeIn(500);
     } else {
-        $("#jquery-description").fadeOut(300);
+        $("#jquery-description").fadeOut(500);
     }
   })
 
