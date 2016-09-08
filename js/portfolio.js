aff1886095aa500bf2f107f1dcc0d8a2bc338e67
rizbo61 <boris.muminovic@gmail.com>
2016-09-08 14:47:33 +0200

    v 0.67

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,21 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  /*$("button").click(function() {
-    $("#description").show().children().hide();
-  })*/
-  
 
-  $("#web-development").click(function() {
+  $("#web-development").click(function () {
     if ($("#web-development-description").css("display") === "none") {
       $("#skills").css("padding", "0");
       $("#description").show().children().hide();
       $("#web-development-description").fadeIn(500);
     } else {
-        $("#web-development-description").fadeOut(500);
+      $("#web-development-description").fadeOut(500);
     }
   })
 
-  $("#html").click(function() {
+  $("#html").click(function () {
     if ($("#html-description").css("display") === "none") {
       $("#skills").css("padding", "0");
       $("#description").show().children().hide();
@@ -25,46 +21,71 @@ $(document).ready(function() {
     }
   })
 
-   $("#css").click(function() {
-    if ($("#css-description").css("display") === "none") { 
+  $("#css").click(function () {
+    if ($("#css-description").css("display") === "none") {
       $("#skills").css("padding", "0");
       $("#description").show().children().hide();
       $("#css-description").fadeIn(500);
     } else {
-        $("#css-description").fadeOut(500);
+      $("#css-description").fadeOut(500);
     }
   })
 
-  $("#javascript").click(function() {
+  $("#javascript").click(function () {
     if ($("#javascript-description").css("display") === "none") {
       $("#skills").css("padding", "0");
       $("#description").show().children().hide();
       $("#javascript-description").fadeIn(500);
     } else {
-        $("#javascript-description").fadeOut(500);
+      $("#javascript-description").fadeOut(500);
     }
   })
 
-  $("#jquery").click(function() {
+  $("#jquery").click(function () {
     if ($("#jquery-description").css("display") === "none") {
       $("#skills").css("padding", "0");
       $("#description").show().children().hide();
       $("#jquery-description").fadeIn(500);
     } else {
-        $("#jquery-description").fadeOut(500);
+      $("#jquery-description").fadeOut(500);
     }
   })
-  
+
 
 
   var offset = 50;
- 
-  $(window).scroll(function() {
+
+  $(window).scroll(function () {
     if ($(this).scrollTop() > offset) {
       $("#go-top").css("opacity", "1");
     } else {
       $("#go-top").css("opacity", "0");
     }
   })
- 
+
+  $("#go-top").click(function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, 300);
+    return false;
+  })
+  $("#go-about").click(function (e) {
+    e.preventDefault();
+    var about = $("#about").offset();
+    $("html, body").animate({ scrollTop: about.top }, 300);
+    return false;
+  })
+  $("#go-projects").click(function (e) {
+    e.preventDefault();
+    var projects = $("#projects").offset();
+    $("html, body").animate({ scrollTop: projects.top }, 300);
+    return false;
+  })
+  $("#go-contact").click(function (e) {
+    e.preventDefault();
+    var contact = $("#contact").offset();
+    $("html, body").animate({ scrollTop: contact.top }, 300);
+    return false;
+  })
+
+
 });
