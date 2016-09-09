f24d6f5b99db3d0ccbe0f1cdfd731a09441c4cf3
rizbo61 <boris.muminovic@gmail.com>
2016-09-09 17:35:01 +0200

    v 0.74

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,89 +1,79 @@
 $(document).ready(function () {
 
 
-  $("#web-development").click(function () {
+  $("#skills > button").click(function() {
     $("#description").show();
     $("#skills").css("padding", "0");
+  });
+
+  $("#web-development").click(function () {
+
     $("button:not(#web-development)").removeClass();
     $(this).blur();
     $(this).stop().toggleClass("teal");
-    $("#description > div:not(#web-development-description)").fadeOut(300).promise().done(function () {
 
-      $("#web-development-description").fadeToggle(300);
+    $("#description > div").stop(true, true);   // STOPS THE ANIMATION OVERLAP!!
+    $("#description > div:not(#web-development-description)").fadeOut(200).promise().done(function () {
+      $("#web-development-description").fadeToggle(200);
     });
 
-    /* if ($("#web-development-description").css("display") === "none") {
-       $("#skills").css("padding", "0");
-       $("#description").show().children().hide();
-       $("#web-development-description").fadeIn(500);
-     } else {
-       $("#web-development-description").fadeOut(500);
-     }*/
   });
 
   $("#html").click(function () {
-    $("#description").show();
-    $("#skills").css("padding", "0");
+    
     $("button:not(#html)").removeClass();
     $(this).blur();
     $(this).stop().toggleClass("purple");
-    $("#description > div:not(#html-description)").fadeOut(300).promise().done(function () {
-
-      $("#html-description").fadeToggle(300);
+    
+    $("description > div").stop(true, true);
+    $("#description > div:not(#html-description)").fadeOut(200).promise().done(function () {
+      $("#html-description").fadeToggle(200);
     });
 
-
-    /*if ($("#html-description").css("display") === "none") {
-      $("#skills").css("padding", "0");
-      $("#description").show().children().hide();
-      $("#html-description").fadeIn(500);
-    } else {
-      $("#html-description").fadeOut(500);
-    }*/
   });
 
   $("#css").click(function () {
-    $("#description").show();
-    $("#skills").css("padding", "0");
+   
     $("button:not(#css)").removeClass();
     $(this).blur();
     $(this).stop().toggleClass("yellow");
-    $("#description > div:not(#css-description)").fadeOut(300).promise().done(function () {
 
-      $("#css-description").fadeToggle(300);
+    $("description > div").stop(true, true);
+    $("#description > div:not(#css-description)").fadeOut(200).promise().done(function () {
+      $("#css-description").fadeToggle(200);
     });
 
   });
 
   $("#javascript").click(function () {
-    $("#description").show();
-    $("#skills").css("padding", "0");
+    
     $("button:not(#javascript)").removeClass();
     $(this).blur();
     $(this).stop().toggleClass("blue");
-    $("#description > div:not(#javascript-description)").fadeOut(300).promise().done(function () {
 
-      $("#javascript-description").fadeToggle(300);
+    $("description > div").stop(true, true);
+    $("#description > div:not(#javascript-description)").fadeOut(200).promise().done(function () {
+      $("#javascript-description").fadeToggle(200);
     });
 
   });
 
   $("#jquery").click(function () {
-    $("#description").show();
-    $("#skills").css("padding", "0");
+    
     $("button:not(#jquery)").removeClass();
     $(this).blur();
     $(this).stop().toggleClass("orange");
-    $("#description > div:not(#jquery-description)").fadeOut(300).promise().done(function () {
 
-      $("#jquery-description").fadeToggle(300);
+    $("description > div").stop(true, true);
+    $("#description > div:not(#jquery-description)").fadeOut(200).promise().done(function () {
+      $("#jquery-description").fadeToggle(200);
     });
 
   });
 
   $(document).click(function () {
     $("button").removeClass();
-    $("#description > div").fadeOut(500);
+    $("#description > div").fadeOut(200);
   });
 
   $("button").click(function (e) {
